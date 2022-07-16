66ecba63a94e1f6fff443b0d77c7c028fecfe0c4
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-07-16 16:06:56 +0300

    xamarin project

---
+++ RestFulProject/RestFulProject/RestFulProject/Services/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestFulProject.Services
+{
+    public class Constants
+    {
+        public static string BaseUserAdress = "http://192.168.1.102/XamarinRest/api/user";
+    }
+}
